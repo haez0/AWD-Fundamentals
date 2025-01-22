9b638b33c50e382d26b729921cd4b4f80cdf9c80
Hannah <nahrain.escarez@gmail.com>
2025-01-23 02:23:36 +0800

    changes for jan 23

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ document.getElementById("greet-btn").addEventListener("click", function ()
 
 //condi statements
 document.getElementById("check-btn").addEventListener("click", function () {
+  //gets the value entered in the "guess" input bar which is a string and converts it to a number using rhe Number() function.
   const userGuess = Number(document.getElementById("guess").value);
   if (userGuess === randomNumber) {
     document.getElementById("result").textContent = "Correct! You guessed the number!";
@@ -29,9 +30,10 @@ document.getElementById("countdown-btn").addEventListener("click", function ()
 {
   const countdownList = document.getElementById("countdown");
   countdownList.innerHTML = "";//to clear prev countdown
+  //loop starts at 5 and counts down to 1, decreasing the value of i by 1 on each iteration.
   for (let i = 5; i >0; i--){
     const listItem = document.createElement("li")
-    listItem.textContent = `Countdown: ${i}`;
+    listItem.textContent = `${i}`;
     countdownList.appendChild(listItem);
   }
 })
